cf9bbe3365e3bb2c219535bde7355229951cb2da
David Hevesi <davidhevesi2008@gmail.com>
2018-10-13 17:55:27 -0400

    separate Form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
-import "./components/Card.js";
+import Form from "./components/Form";
 
 import "./App.css";
 
@@ -28,36 +27,6 @@ const CardList = props => {
     </div>
   );
 };
-
-class Form extends React.Component {
-  state = {
-    userName: ""
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    axios
-      .get(`https://api.github.com/users/${this.state.userName}`)
-      .then(resp => {
-        this.props.onSubmit(resp.data);
-      });
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.userName}
-          onChange={event => this.setState({ userName: event.target.value })}
-          placeholder="Github Username"
-          required
-        />
-        <button type="submit">Add Card Here</button>
-      </form>
-    );
-  }
-}
 
 class App extends Component {
   state = {
